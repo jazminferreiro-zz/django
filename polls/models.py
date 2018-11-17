fe26f7c0a15a870cfb2648f24b54944d39492db2
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-11-17 18:44:24 -0300

    parte3

--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,6 +16,7 @@ class Question(models.Model):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
+@python_2_unicode_compatible 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
-#url(regex para matchear con la url, view que va a llamr, nombre)
-
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
